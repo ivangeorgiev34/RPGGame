c252d77cefbb47d453bd3e1a7217f52084dc3670
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 00:20:07 +0200

    Add Archer class

---
+++ RPGGame.Infrastructure/Models/Archer.cs
@@ -0,0 +1,12 @@
+﻿namespace RPGGame.Infrastructure.Models
+{
+    public class Archer : Creature
+    {
+        public override char Symbol => '#';
+
+        public override int Strenght { get; set; } = 2;
+        public override int Agility { get; set; } = 4;
+        public override int Intelligence { get; set; } = 0;
+        public override int Range { get; set; } = 2;
+    }
+}
