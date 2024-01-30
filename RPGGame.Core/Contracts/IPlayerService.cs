81ce1537ed344a61c84cd3affa442680e926cb69
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-30 22:41:16 +0200

    Remove AddPointsToPlayer method from IPlayerService

--- RPGGame.Core/Contracts/IPlayerService.cs
+++ RPGGame.Core/Contracts/IPlayerService.cs
@@ -3,7 +3,5 @@
     public interface IPlayerService
     {
         public void AddPointsToPlayerProperty(int points, string currentProperty, string[] heroProperties);
-
-        public void AddPointsToPlayer(int remainingPoints, int remainingPointsConsoleRowPosition, int remainingPointsConsoleColPosition);
     }
 }
