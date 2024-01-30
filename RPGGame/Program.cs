8206bad9e006625c3a7fdc9898cb10eb713a90d8
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-30 17:00:25 +0200

    Add ShowMainMenu invocation

--- RPGGame/Program.cs
+++ RPGGame/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace RPGGame
+﻿using RPGGame.Contracts;
+using RPGGame.Enums;
+using RPGGame.Models;
+
+namespace RPGGame
 {
     internal class Program
     {
+        public static Screen currentScreen = Screen.MainMenu;
+        private static IScreenManager screenManager = new ConsoleScreenManager();
+        private static Creature? player;
+
         static void Main(string[] args)
         {
+            screenManager.ShowMainMenu();
 
         }
     }
