f29a7fba817e76e8eed741f79c4b2e9297dc6c2e
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-30 22:33:31 +0200

    Add AddPointsToPlayerProperty method to IPlayerService interface

---
+++ RPGGame.Core/Contracts/IPlayerService.cs
@@ -0,0 +1,8 @@
+﻿namespace RPGGame.Core.Contracts
+{
+    public interface IPlayerService
+    {
+        public void AddPointsToPlayerProperty(int points, string currentProperty, string[] heroProperties);
+
+    }
+}
