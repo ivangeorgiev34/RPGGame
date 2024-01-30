c18f35e0eb7f38201b898512a1ed05338eab5520
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-30 15:36:21 +0200

    Add ShowMainMenu method implementation

---
+++ RPGGame/ConsoleScreenManager.cs
@@ -0,0 +1,21 @@
+﻿using RPGGame.Contracts;
+using RPGGame.Enums;
+
+namespace RPGGame
+{
+    public class ConsoleScreenManager : IScreenManager
+    {
+        public void ShowMainMenu()
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Press any key to play!");
+
+            var key = Console.ReadKey(true);
+
+            if (key.Key.ToString() != "")
+            {
+                Program.currentScreen = Screen.CharacterSelect;
+            }
+        }
+    }
+}
