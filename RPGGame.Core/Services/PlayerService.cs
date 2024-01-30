1760485ba7baae6fa74647ef59efcbfe783117c4
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-30 22:34:07 +0200

    Add AddPointsToPlayerProperty method implementation in PlayerService

---
+++ RPGGame.Core/Services/PlayerService.cs
@@ -0,0 +1,27 @@
+﻿using RPGGame.Core.Contracts;
+
+namespace RPGGame.Core.Services
+{
+    public class PlayerService : IPlayerService
+    {
+
+        public void AddPointsToPlayerProperty(int points, string currentProperty, string[] heroProperties)
+        {
+            if (Program.player != null)
+            {
+                if (currentProperty == heroProperties[0])
+                {
+                    Program.player.Strenght = points;
+                }
+                else if (currentProperty == heroProperties[1])
+                {
+                    Program.player.Agility = points;
+                }
+                else if (currentProperty == heroProperties[2])
+                {
+                    Program.player.Intelligence = points;
+                }
+            }
+        }
+    }
+}
