7113fe61fe5552935e8dc2a1cc5aaaeda06ed9d8
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 00:20:33 +0200

    Add Mage class

---
+++ RPGGame.Infrastructure/Models/Mage.cs
@@ -0,0 +1,12 @@
+﻿namespace RPGGame.Infrastructure.Models
+{
+    public class Mage : Creature
+    {
+        public override char Symbol => '*';
+
+        public override int Strenght { get; set; } = 2;
+        public override int Agility { get; set; } = 1;
+        public override int Intelligence { get; set; } = 3;
+        public override int Range { get; set; } = 3;
+    }
+}
