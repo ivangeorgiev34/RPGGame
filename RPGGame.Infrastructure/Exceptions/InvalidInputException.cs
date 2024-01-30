1ed3c734a867b253d12e57f4f94b95dfdad579ae
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 00:19:50 +0200

    Add InvalidInputException custom exception class

---
+++ RPGGame.Infrastructure/Exceptions/InvalidInputException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace RPGGame.Infrastructure.Exceptions
+{
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+        {
+        }
+
+        public InvalidInputException(string? message) : base(message)
+        {
+        }
+
+        public InvalidInputException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidInputException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
