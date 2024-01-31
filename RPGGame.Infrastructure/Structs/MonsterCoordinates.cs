b2af3a8ea90c17c0ad6cc5c55c3e60fe6fd7afb2
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 17:14:21 +0200

    Add row and column constructor in MonsterCoordinates struct

--- RPGGame.Infrastructure/Structs/MonsterCoordinates.cs
+++ RPGGame.Infrastructure/Structs/MonsterCoordinates.cs
@@ -2,6 +2,12 @@
 {
     public struct MonsterCoordinates
     {
+        public MonsterCoordinates(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
         public int Row { get; set; }
         public int Column { get; set; }
     }
