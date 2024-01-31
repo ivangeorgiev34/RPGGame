3805086024aa4a83cfa88cc1e4aeda5fd6b823c9
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 13:15:09 +0200

    Add RPGGameDbContext db context class

---
+++ RPGGame.Infrastructure/Data/RPGGameDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPGGame.Infrastructure.Models;
+
+namespace RPGGame.Infrastructure.Data
+{
+    public class RPGGameDbContext : DbContext
+    {
+        public RPGGameDbContext()
+        {
+        }
+
+        public RPGGameDbContext(DbContextOptions<RPGGameDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player>? Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=DESKTOP-1FK883M\\SQLEXPRESS;Database=RPGGame;Trusted_Connection=True;");
+            }
+        }
+    }
+}
