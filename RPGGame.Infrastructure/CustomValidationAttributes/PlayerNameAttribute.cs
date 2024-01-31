f893f13c05da959717abba40244f7ac4b84c0bdf
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 13:14:32 +0200

    Add PlayerNameAttribute custom validation attribute

---
+++ RPGGame.Infrastructure/CustomValidationAttributes/PlayerNameAttribute.cs
@@ -0,0 +1,23 @@
+﻿using RPGGame.Infrastructure.Constants;
+using RPGGame.Infrastructure.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPGGame.Infrastructure.CustomValidationAttributes
+{
+    public class PlayerNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var name = value?.ToString();
+
+            if (name != nameof(Warrior)
+                && name != nameof(Archer)
+                && name != nameof(Mage))
+            {
+                return new ValidationResult(InfrastructureConstants.Player.PLAYER_NAME_ERROR_MESSAGE);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
