35a25907605a5344918b002429634f51b9e6a832
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2024-01-31 11:40:41 +0200

    Add InfrastructureConstants class for constants

---
+++ RPGGame.Infrastructure/Constants/InfrastructureConstants.cs
@@ -0,0 +1,41 @@
+﻿namespace RPGGame.Infrastructure.Constants
+{
+    public class InfrastructureConstants
+    {
+        public static class Player
+        {
+            public const string PLAYER_CLASS_COMMENT = "Players";
+
+            public const string PLAYER_ID_COMMENT = "Player identificator";
+
+            public const string PLAYER_NAME_REQUIRED_ERROR_MESSAGE = "Player name is required!";
+            public const string PLAYER_NAME_COMMENT = "Player name";
+            public const string PLAYER_NAME_ERROR_MESSAGE = "You can choose only between Warrior, Mage, and Archer!";
+
+            public const string PLAYER_DATE_CREATED_REQUIRED_ERROR_MESSAGE = "Player name is required!";
+            public const string PLAYER_DATE_CREATED_COMMENT = "Player date created";
+
+            public const string PLAYER_HEALTH_REQUIRED_ERROR_MESSAGE = "Player health is required!";
+            public const string PLAYER_HEALTH_COMMENT = "Player health";
+
+
+            public const string PLAYER_MANA_REQUIRED_ERROR_MESSAGE = "Player mana is required!";
+            public const string PLAYER_MANA_COMMENT = "Player mana";
+
+            public const string PLAYER_DAMAGE_REQUIRED_ERROR_MESSAGE = "Player damage is required!";
+            public const string PLAYER_DAMAGE_COMMENT = "Player damage";
+
+            public const string PLAYER_STRENGHT_REQUIRED_ERROR_MESSAGE = "Player strenght is required!";
+            public const string PLAYER_STRENGHT_COMMENT = "Player strenght";
+
+            public const string PLAYER_AGILITY_REQUIRED_ERROR_MESSAGE = "Player agility is required!";
+            public const string PLAYER_AGILITY_COMMENT = "Player agility";
+
+            public const string PLAYER_INTELLIGENCE_REQUIRED_ERROR_MESSAGE = "Player intelligence is required!";
+            public const string PLAYER_INTELLIGENCE_COMMENT = "Player intelligence";
+
+            public const string PLAYER_RANGE_REQUIRED_ERROR_MESSAGE = "Player range is required!";
+            public const string PLAYER_RANGE_COMMENT = "Player range";
+        }
+    }
+}
